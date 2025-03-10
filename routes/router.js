fcca3999c42d94f28fe06367ef5a989d987a3913
bojler elado <116706752+bojlerelado13245@users.noreply.github.com>
2025-03-10 14:08:58 +0100

    Add files via upload

---
+++ routes/router.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const router = express.Router();
+
+let wizards = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    magic_wand: "long",
+    house_name: "John Doe way 12",
+    free: false,
+  },
+];
+
+router.get("/wizards", (req, res) => {
+  res.send(wizards);
+});
+
+router.get("/wizards/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.json(wizards.find((wizard) => wizard.id == id));
+});
+
+router.post("/wizards", (req, res) => {
+  const { name, magic_wand, house_name, free } = req.body;
+  wizards.push({
+    id: wizards[wizards.length-1].id + 1 , 
+    name,
+    magic_wand,
+    house_name,
+    free,
+  });
+
+  res.send(wizards);
+});
+
+router.put("/wizards/:id", (req, res) => {
+  const { name, magic_wand, house_name, free } = req.body;
+  const id = Number(req.params.id);
+  const wizardIndex = wizards.findIndex((wizard) => wizard.id == id);
+
+  wizards[wizardIndex] = { id, name, magic_wand, house_name, free };
+
+  res.send(wizards[wizardIndex]);
+});
+
+router.delete("/wizards/:id", (req, res) => {
+  const id = req.params.id;
+
+  wizards = wizards.filter((wizard) => wizard.id != id);
+
+  res.send(wizards);
+});
+
+export default router;
